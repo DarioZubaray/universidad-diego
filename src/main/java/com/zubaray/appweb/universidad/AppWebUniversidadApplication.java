35baa540f2c29c271c23ee65dee7ef201c23dab2
Dario Zubaray <dzubaray@gmail.com>
2021-03-21 14:12:27 -0300

    adding empty springboot project

---
+++ src/main/java/com/zubaray/appweb/universidad/AppWebUniversidadApplication.java
@@ -0,0 +1,13 @@
+package com.zubaray.appweb.universidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebUniversidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebUniversidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zubaray/appweb/universidad/AppWebUniversidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zubaray.appweb.universidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebUniversidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
